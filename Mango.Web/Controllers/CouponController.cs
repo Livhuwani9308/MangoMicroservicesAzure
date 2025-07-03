fc7ff5de2bb6d41d061eea1ca6d4ff26331eaabf
LivhuwaniKM <lkmasindi1208@gmail.com>
2025-07-03 15:37:53 +0200

    Coupon List & Create Methods | Views

---
+++ Mango.Web/Controllers/CouponController.cs
@@ -0,0 +1,44 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class CouponController(ICouponService _couponService) : Controller
+    {
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto> list = [];
+
+            ResponseDto? response = await _couponService.GetAllCouponsAsync();
+
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<CouponDto>>(response.Result.ToString());
+            }
+
+            return View(list);
+        }
+
+        public async Task<IActionResult> CouponCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _couponService.CreateCouponsAsync(model);
+
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+            return View(model);
+        }
+    }
+}
