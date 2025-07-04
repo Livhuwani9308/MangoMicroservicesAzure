b014fb59e261b839af61e0996f5ac6b37dd6362e
LivhuwaniKM <lkmasindi1208@gmail.com>
2025-07-04 14:16:03 +0200

    Auth API | DbContext | IdentityDbContext  | Identity Tables Creation

---
+++ Mango.Services.AuthAPI/Program.cs
@@ -0,0 +1,52 @@
+using Mango.Services.AuthAPI.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+ApplyMigration();
+
+app.Run();
+
+void ApplyMigration()
+{
+    using var scope = app.Services.CreateScope();
+
+    var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    if (_db.Database.GetPendingMigrations().Count() > 0)
+    {
+        _db.Database.Migrate();
+    }
+}
