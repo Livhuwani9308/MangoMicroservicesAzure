2e09c98670f98e26bec7426ae6dab09f78ab0e5c
LivhuwaniKM <lkmasindi1208@gmail.com>
2025-07-16 13:55:41 +0200

    Shopping Cart API | DbContext | Environment Variables | CRUD | Authentication | AutoMapper

---
+++ Mango.Services.ShoppingCartAPI/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Mango.Services.ShoppingCartAPI.Extensions
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        public static WebApplicationBuilder AddAppAuthentication(this WebApplicationBuilder builder)
+        {
+            var settingsSection = builder.Configuration.GetSection("ApiSettings");
+
+            var secret = settingsSection.GetValue<string>("Secret");
+            var audience = settingsSection.GetValue<string>("Audience");
+            var issuer = settingsSection.GetValue<string>("Issuer");
+
+            var key = Encoding.ASCII.GetBytes(secret);
+
+            builder.Services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(opt =>
+            {
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = issuer,
+                    ValidAudience = audience,
+                };
+            });
+            return builder;
+        }
+    }
+}
